745c8f00f8f9801079c6672a0c0dba60c1df6ff4
Michael <mokchira@gmail.com>
2021-08-07 17:22:29 -0400

    added macro header

---
+++ src/include/hell/macro.h
@@ -0,0 +1,11 @@
+#define HELL_INIT(framefn) \
+    eventQueue = hell_AllocEventQueue();\
+    grimoire   = hell_AllocGrimoire();\
+    console    = hell_AllocConsole();\
+    window     = hell_AllocWindow();\
+    hellmouth  = hell_AllocHellmouth();\
+    hell_CreateConsole(console);\
+    hell_CreateEventQueue(eventQueue);\
+    hell_CreateGrimoire(eventQueue, grimoire);\
+    hell_CreateWindow(eventQueue, 666, 666, NULL, window);\
+    hell_CreateHellmouth(grimoire, eventQueue, console, 1, &window, framefn, NULL, hellmouth)\
