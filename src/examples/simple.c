0e22f29b16a311278c31bfcb4cfe31af1d786cf3
mokchira <mokchira@gmail.com>
2022-03-06 08:25:06 -0500

    Updated example

--- src/examples/simple.c
+++ src/examples/simple.c
@@ -1,17 +1,28 @@
 #include "hell/common.h"
 
+Hell_Hellmouth* hellmouth;
+
+void frame(i64 fi, i64 dt)
+{
+    int evcount;
+    const Hell_Event* events = hell_GetEvents(hellmouth, &evcount);
+    hell_Print("Event count %d\n", evcount);
+    if (fi > 400)
+        hell_CloseAndExit(hellmouth);
+}
+
 int hellmain(void)
 {
     Hell_Grimoire*   grimoire   = hell_Malloc(hell_SizeOfGrimoire());
     Hell_Console*    console    = hell_Malloc(hell_SizeOfConsole());
     Hell_EventQueue* eventQueue = hell_Malloc(hell_SizeOfEventQueue());
     Hell_Window*     window     = hell_Malloc(hell_SizeOfWindow());
-    Hell_Hellmouth*  hellmouth  = hell_Malloc(hell_SizeOfHellmouth());
+    hellmouth  = hell_Malloc(hell_SizeOfHellmouth());
     hell_CreateEventQueue(eventQueue);
     hell_CreateGrimoire(eventQueue, grimoire);
     hell_CreateConsole(console);
     hell_CreateWindow(eventQueue, 500, 500, NULL, window);
-    hell_CreateHellmouth(grimoire, eventQueue, console, 1, &window, NULL, NULL, hellmouth);
+    hell_CreateHellmouth(grimoire, eventQueue, console, 1, &window, frame, NULL, hellmouth);
     hell_Loop(hellmouth);
     return 0;
 }
